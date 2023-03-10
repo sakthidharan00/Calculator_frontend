003bfdb2c78c2b096c2e04d1d69a14cd5d4738c2
Sakthi <sakthidharan00@gmail.com>
2023-03-10 19:13:07 +0530

    css styles modified

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
 
   const equal = async () => {
-    if (value ===  0) {
+    if (value === 0) {
       alert("Invalid Input....")
     }
     else {
@@ -56,127 +56,128 @@ function App() {
   return (
     <div className="container-fluid">
       <h3 className="bg-primary">Calculator</h3>
-      <div className=" container calculator ">
-        {/* row1 */}
-        <div className="row">
+      <div className='calculator-content'>
+        <div className=" container calculator ">
+          {/* row1 */}
+          <div className="row">
 
-          <div className="input-group col" >
-            <p id="his-dis" className='text-muted' ><HistoryIcon  /> {history}</p>
-            <input className="form-control" type="text" placeholder="0" value={value} disabled />
+            <div className="input-group col" >
+              <p id="his-dis" className='text-muted' ><HistoryIcon /> {history}</p>
+              <input className="form-control" type="text" placeholder="0" value={value} disabled />
+            </div>
+
           </div>
+          <div className="button">
+
+            {/* row 2 */}
+            <div className="row">
+              <div className="col-3">
+                <button onClick={() => setValue(history)}><HistoryIcon /></button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>/</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>*</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)} >-</button>
+              </div>
+
+            </div>
+
+
+
+
+
+
+            {/* row 3 */}
+            <div className="row">
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>7</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>8</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>9</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>+</button>
+              </div>
+
+            </div>
+
+
+
+
+
+
+
+
+            {/* row 4 */}
+
+            <div className="row">
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>4</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>5</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>6</button>
+              </div>
+              <div className="col-3">
+                <button onClick={() => setValue(value.slice(0, -1))}>Del</button>
+              </div>
+
+            </div>
+
+
+
+            {/* row 5 */}
+
+            <div className="row">
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>1</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>2</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>3</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>.</button>
+              </div>
+
+            </div>
+
+
+
+
+
+            {/* row 6 */}
+
+            <div className="row">
+              <div className="col-3">
+                <button onClick={() => setValue("")}>Clr</button>
+              </div>
+              <div className="col-3">
+                <button onClick={(e) => setValue(value += e.target.innerText)}>0</button>
+              </div>
+
+              <div className="col-6 ">
+                <button className="equals" onClick={equal} >=</button>
+              </div>
+
+            </div>
+
+          </div>
+
+
 
         </div>
-        <div className="button">
-
-          {/* row 2 */}
-          <div className="row">
-            <div className="col-3">
-              <button onClick={() => setValue(history)}><HistoryIcon /></button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>/</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>*</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)} >-</button>
-            </div>
-
-          </div>
-
-
-
-
-
-
-          {/* row 3 */}
-          <div className="row">
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>7</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>8</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>9</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>+</button>
-            </div>
-
-          </div>
-
-
-
-
-
-
-
-
-          {/* row 4 */}
-
-          <div className="row">
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>4</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>5</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>6</button>
-            </div>
-            <div className="col-3">
-              <button onClick={() => setValue(value.slice(0, -1))}>Del</button>
-            </div>
-
-          </div>
-
-
-
-          {/* row 5 */}
-
-          <div className="row">
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>1</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>2</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>3</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>.</button>
-            </div>
-
-          </div>
-
-
-
-
-
-          {/* row 6 */}
-
-          <div className="row">
-            <div className="col-3">
-              <button onClick={() => setValue("")}>Clr</button>
-            </div>
-            <div className="col-3">
-              <button onClick={(e) => setValue(value += e.target.innerText)}>0</button>
-            </div>
-
-            <div className="col-6 ">
-              <button className="equals" onClick={equal} >=</button>
-            </div>
-
-          </div>
-
-        </div>
-
-
-
-
 
       </div>
 
